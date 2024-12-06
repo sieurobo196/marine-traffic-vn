45dc9bc0940ce618ec5bb4880b5cddaa249320d2
Sieurobo196 <32062493+sieurobo196@users.noreply.github.com>
2024-12-06 19:04:24 +0700

    Create CommonConstant.java

---
+++ marine-traffic-backend/src/main/java/com.marine.traffic/constant/CommonConstant.java
@@ -0,0 +1,9 @@
+package com.marine.traffic.constant;
+
+public final class CommonConstant {
+    public static final String JWT_REDIS_KEY = "jwt__%s";
+    public static final String FAILED = "FAILED";
+    public static final String SUCCESS = "SUCCESS";
+
+    public final static String OK = "OK";
+}
