bf70646f3332dfe245fd23c36098a304dc70a705
Sieurobo196 <32062493+sieurobo196@users.noreply.github.com>
2024-12-06 19:04:44 +0700

    Create CommonMySQL.java

---
+++ marine-traffic-backend/src/main/java/com.marine.traffic/constant/CommonMySQL.java
@@ -0,0 +1,10 @@
+package com.marine.traffic.constant;
+
+/**
+ * This file should be handled to read all sql files which are placed under "integration/common" directory
+ */
+public class CommonMySQL {
+
+    public static final String GET_LIST_PORTS = "mysql/get_list_ports";
+
+}
