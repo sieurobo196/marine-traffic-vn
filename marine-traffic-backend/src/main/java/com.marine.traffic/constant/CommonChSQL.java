1ea548d9b7af2f5342e070464e0c4ef5b8bb4eb1
Sieurobo196 <32062493+sieurobo196@users.noreply.github.com>
2024-12-06 19:04:07 +0700

    Create CommonChSQL.java

---
+++ marine-traffic-backend/src/main/java/com.marine.traffic/constant/CommonChSQL.java
@@ -0,0 +1,13 @@
+package com.marine.traffic.constant;
+
+/**
+ * This file should be handled to read all sql files which are placed under "integration/common" directory
+ */
+public class CommonChSQL {
+
+    public static final String INSERT_MARINE_TRAFFIC = "clickhouse/detail/insert_marine_traffic";
+    public static final String GET_MARINE_TRAFFIC = "clickhouse/detail/get_marine_traffic";
+    public static final String GET_MARINE_TRAFFIC_DETAIL = "clickhouse/detail/get_marine_traffic_detail";
+    public static final String GET_LIST_PORTS = "clickhouse/detail/get_list_ports";
+
+}
